39332c7cb1babc91cb3371a5e56cabf4b55c0882
zahara love <z_love@coloradocollege.edu>
2025-11-17 19:39:09 -0700

    Added API call functionality for fetching hotspot info for a given date

--- server/src/main.py
+++ server/src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
-import os
-
+import os,httpx
+from datetime import datetime, timedelta, date
 
 load_dotenv()
 app = FastAPI()
@@ -10,11 +10,46 @@ app = FastAPI()
 def read_root():
     return {"message": "backend api is online. (:"}
 
+
 '''
-# example on how to grab key: value loads at serverip/debug-key
-# dont keep this for security reasons
-@app.get("/debug-key")
-def check_key():
-    key = os.getenv("EBIRD_API_KEY")
-    return {"key-status": "loaded" if key else "missing"}
+Returns info for the given hotspot for a given date range. 
+If no range is given default to returning info for entire range of dates available (1800-present).
+
+Values for Testing: 
+
+L1150539 -hotspotID
+2017-06-01 - start date
+2017-08-30 - end date
 '''
+@app.get("/hotspot")
+async def get_hotspot_obs(hotspotID:str, startDate:str = None,endDate:str = None):
+    headers = {"X-eBirdApiToken":os.getenv("EBIRD_API_KEY")}
+
+    if startDate and endDate:
+        return await get_dateRange_obs(hotspotID,startDate, endDate)
+    else:
+        return await get_dateRange_obs(hotspotID,"1800-01-01", date.today())
+
+@app.get("/hotspot/daterange")
+async def get_dateRange_obs(hotspotID,startDate,endDate):
+    headers = {"X-eBirdApiToken":os.getenv("EBIRD_API_KEY")}
+    results = []
+
+    sd = datetime.strptime(startDate,"%Y-%m-%d").date()
+    ed = datetime.strptime(endDate,"%Y-%m-%d").date()
+
+    async with httpx.AsyncClient() as client:
+        while sd <= ed:
+            year,month,day = sd.year,sd.month,sd.day
+            url = f"https://api.ebird.org/v2/data/obs/{hotspotID}/historic/{year}/{month}/{day}"
+
+            response = await client.get(url, headers=headers)
+            response.raise_for_status()
+
+            formatted = response.json()
+            results.extend(formatted)
+            sd += timedelta(days=1)
+
+    return {'hotspot data' : results}
+
+# async def get_top_birds(numBirds:int):
