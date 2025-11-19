c1bb77e7ade7bf70502e8fe969f3b40a5cb172d5
erinliedeker <e_liedeker@coloradocollege.edu>
2025-11-19 12:02:58 -0700

    placeholder screens set up for hotspot search/detailed view

--- client/src/views/HotspotDetail.vue
+++ client/src/views/HotspotDetail.vue
@@ -2,15 +2,29 @@
   <div class="hotspot-detail">
     <h1>Hotspot Detail</h1>
     <p>This is the placeholder for the hotspot detail screen.</p>
+    <button class="button" @click="navigateToHotspotSearch">
+      <span>Go to hotspot search screen</span>
+    </button>
     <!-- Future: graphs, images, lists of birds, etc -->
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
 
 export default defineComponent({
   name: 'HotspotDetail',
+  setup() {
+    const router = useRouter();
+
+    const navigateToHotspotSearch = () => {
+      // Example: navigate to hotspot with ID 1
+      router.push({ name: 'HotspotSearch', params: { id: 1 } });
+    };
+
+    return { navigateToHotspotSearch };
+  },
 });
 </script>
 
--- client/src/views/HotspotSearch.vue
+++ client/src/views/HotspotSearch.vue
@@ -2,15 +2,28 @@
   <div class="hotspot-search">
     <h1>Hotspot Search</h1>
     <p>This is the placeholder for the hotspot search screen.</p>
-    <!-- Future: search input, list of hotspots -->
+    <button class="button" @click="navigateToHotspotDetail">
+      <span>Go to hotspot detail screen</span>
+    </button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { useRouter } from 'vue-router';
 
 export default defineComponent({
   name: 'HotspotSearch',
+  setup() {
+    const router = useRouter();
+
+    const navigateToHotspotDetail = () => {
+      // Example: navigate to hotspot with ID 1
+      router.push({ name: 'HotspotDetail', params: { id: 1 } });
+    };
+
+    return { navigateToHotspotDetail };
+  },
 });
 </script>
 
