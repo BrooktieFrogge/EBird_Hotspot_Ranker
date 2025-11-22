cd2434878ff7f8b5768959d72970594e9b5afc19
Jevon Lipsey <jevonlipsey1029@gmail.com>
2025-11-22 02:58:15 -0700

    add batch fetching. had to seperate session/cookie pulling from data pulling in data_request for speed. they live in their own functions now and playwright(browser) instance lives in  live_ranker.

--- server/src/data_processing/data_request.py
+++ server/src/data_processing/data_request.py
@@ -17,7 +17,8 @@ SESSION_FILE = os.getenv('SESSION_FILE')
 # set to false to watch what the browsers are doing
 HEADLESS = False
 
-## helper function to check if we need to get cookies again
+##### helper functions
+# check if we need to get cookies again
 def is_session_valid(browser):
     # do the cookies exist
     if not os.path.exists(SESSION_FILE):
@@ -34,66 +35,67 @@ def is_session_valid(browser):
     finally:
         if 'context' in locals():
             context.close()
+            
+# get new cookies via playwright
+def ensure_session(browser):
+    if is_session_valid(browser):
+        print("[cookies] | existing session ok. using saved cookies.")
+        return
 
-### main function: go to eBird, login if needed, and fetch data
-def fetch_data(loc, start, end):
-    print(f"[input] | connecting to eBird for {loc} ({start}-{end})...")
+    print("[cookies] | session expired. autologging into ebird to restore cookies...")
 
-    with sync_playwright() as p:
-        browser = p.chromium.launch(headless=HEADLESS)
+    context = None
+    page = None
+    try:
         context = browser.new_context()
+        page = context.new_page()
+        page.goto('https://secure.birds.cornell.edu/cassso/login?service=https%3A%2F%2Febird.org%2Flogin%2Fcas%3Fportal%3Debird&locale=en_US')
 
-        ## restore session if cookies expired
-        if not is_session_valid(browser):
-            print("[cookies] | session expired. autologging into ebird to restore cookies...")
+        page.wait_for_selector('input[name="username"]', timeout=5000)
 
-            page = context.new_page()
-            page.goto('https://secure.birds.cornell.edu/cassso/login?service=https%3A%2F%2Febird.org%2Flogin%2Fcas%3Fportal%3Debird&locale=en_US')
+        ## automated login
+        print("[playwright] | typing .env username...")
+        page.click('input[name="username"]') 
+        page.fill('input[name="username"]', EBIRD_USERNAME)
+        time.sleep(1) 
 
-            try:
-                page.wait_for_selector('input[name="username"]', timeout=5000)
-                
-                ## automated login
-                print("[playwright] | typing .env username...")
-                page.click('input[name="username"]') 
-                page.fill('input[name="username"]', EBIRD_USERNAME)
-                time.sleep(1) 
+        page.keyboard.press("Tab")
 
-                page.keyboard.press("Tab")
+        print("[playwright] | typing .env password...")
+        page.fill('input[name="password"]', EBIRD_PASSWORD)
+        time.sleep(1) 
 
-                print("[playwright] | typing .env password...")
-                page.fill('input[name="password"]', EBIRD_PASSWORD)
-                time.sleep(1) 
+        page.keyboard.press("Enter")
 
-                page.keyboard.press("Enter")
-                
-                print("[playwright] | submitted. redirecting...")
-                time.sleep(3) # wait for redirect
+        print("[playwright] | submitted. redirecting...")
+        time.sleep(3) # wait for redirect
 
-                # save the cookies
-                context.storage_state(path=SESSION_FILE)
-                print("[cookies] | playwright login success! session saved.")
+        # save the cookies
+        context.storage_state(path=SESSION_FILE)
+        print("[cookies] | playwright login success! session saved.")
 
-            except Exception as e:
-                print(f"[error] | .env login skipped or failed: {e}")
-            finally:
-                page.close()
-                context.close()
-        else:
-            print("[cookies] | existing session ok. using saved cookies.")
+    except Exception as e:
+        print(f"[error] | .env login skipped or failed: {e}")
+    finally:
+        if page: page.close()
+        if context: context.close()
 
-        ### fetch data
-        print("[info] | downloading data...")
-        
+### main data request function
+def fetch_data(browser, loc, start, end):
+    print(f"[input] | connecting to eBird for {loc} ({start}-{end})...")
+    print("[info] | downloading data...")
+
+    context = None
+    try:
         context = browser.new_context(storage_state=SESSION_FILE)
         data_url = f"https://ebird.org/barchartData?r={loc}&byr={start}&eyr={end}&bmo=1&emo=12&fmt=tsv"
         response = context.request.get(data_url)
         data_text = response.text()
 
-        # debug
-        browser.close()
         if "<!doctype html>" in data_text.lower():
             print("[error] | something went wrong. either bad location ID or invalid cookies.")
             return None
 
         return data_text
+    finally:
+        if context: context.close()
