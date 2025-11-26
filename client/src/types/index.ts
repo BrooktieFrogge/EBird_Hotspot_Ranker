9e304bd8607482e24790bf08c3f7ce414c76d777
erinliedeker <e_liedeker@coloradocollege.edu>
2025-11-26 16:26:27 -0600

    Implemented custom adding birds to the report

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,4 +1,14 @@
-export interface Point {
-  x: number,
-  y: number
+export interface Bird {
+  species: string,
+  data1: number,
+  data2: number,
+  photo?: string
+}
+
+export interface Hotspot {
+  id: number;
+  name: string;
+  frequency: number;
+  region: string;
+  birds: Bird[];
 }
