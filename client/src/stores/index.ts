a5b88d20bb17265319091255563491fc5e11ef94
erinliedeker <e_liedeker@coloradocollege.edu>
2025-12-04 15:58:48 -0700

    Fixed bug + images are now popping up!

--- client/src/stores/index.ts
+++
@@ -1,7 +0,0 @@
-// central entry point for all Pinia stores
-
-// eg: import stores here as created
-// export { useHotspotStore } from './hotspotStore';
-// export { useUiStore } from './uiStore';
-
-// placeholder so the file exists for now
